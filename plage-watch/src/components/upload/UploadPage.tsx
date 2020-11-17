92e5bc1f59273d0aab8e8297d06b1c709f01c80f
deepak <kumar.de@husky.neu.edu>
2020-11-17 18:13:20 -0500

    added code to display file names

--- plage-watch/src/components/upload/UploadPage.tsx
+++ plage-watch/src/components/upload/UploadPage.tsx
@@ -1,22 +1,47 @@
 import React from "react";
 import './UploadPage.css'
-
+import Dropzone from 'react-dropzone';
+import Drop from './DropZone';
 
 export default class Upload extends React.Component {
+    
+    state = {
+        file1 : '',
+        file2 : '',
+        diabled: true
+    }
+
+
+    runPlagiarism () {
+        console.log("hi");
+    }
+
+
+
     render() {
         return(
-              <div>
-                  <h1 className="center upload-text">Upload Folders To Run Plagiarism!!</h1>
-                 <div className= "">
-                     <div className="row mt-4">
-                     <div className="col-sm-6 center">
-                    <input className="" type="file" accept=".zip"></input>
+              <div className="mt-4">
+                  <h1 className="center  upload-text">Upload Folders To Run Plagiarism!!</h1>
+                 <div className= "container-fluid  row flex">
+                     <div className="col-sm-6  mt-4 center sub-style">      
+                         <Drop/>
+                
+                     </div>
+
+                     <div className="col-sm-6 mt-4 center sub-style">
+                         <Drop/>
+                     </div>
+                    <div className="col-sm-12 mt-4 center">
+
+                    <div className="btn border rounded check-button text-light p-2" onClick={this.runPlagiarism}>    
+                    <i className="fas fa-search"> </i>
+                    <span> Check Plagiarism</span>
                     </div>
-                    <div className="col-sm-6 center">
-                    <input className="center" type="file" accept=".zip"></input>
-                    </div>
+    
+
                      </div> 
-                    <button type="button" className="btn btn-primary center">Run Plagiarism</button>
+                    {/* <button type="button" className="btn btn-primary">Run Plagiarism</button> */}
+                  
                  </div>
               </div>        
         );
