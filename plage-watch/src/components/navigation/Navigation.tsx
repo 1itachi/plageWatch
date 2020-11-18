7b9cdf026a2a54c336330bbe516d4d3fb9610320
Jason Lu <lu.jaso@northeastern.edu>
2020-11-18 11:34:53 -0500

    Added in a login page for the detector.

--- plage-watch/src/components/navigation/Navigation.tsx
+++ plage-watch/src/components/navigation/Navigation.tsx
@@ -3,16 +3,17 @@ import './Navigation.css'
 import detect from './../../images/plagewatch.jpg'
 
 export default class NavBar extends React.Component {
-    render() {
-        return(
+  render() {
+    return (
 
-              <div className="col-sm-6 row center">
-                <img className="ml-4 plagewatch-image-style" src ={detect} alt="detect"/>
-              <h1 className="mt-4 nav-text-style"> PlageWatch</h1>
-              </div>
-
-
-              
-        );
-      }
+      <div className="col-sm-6 row center">
+        <img className="ml-4 plagewatch-image-style" src={detect} alt="detect" />
+        <h1 className="mt-4 nav-text-style"> PlageWatch</h1>
+        <br></br>
+        <h1 className="mt-4 nav-text-style">&nbsp;&nbsp;
+        <i className="fas fa-download"></i></h1>
+        <h1 className="mt-4 nav-text-style">&nbsp;&nbsp;Report</h1>
+      </div>
+    );
+  }
 }
