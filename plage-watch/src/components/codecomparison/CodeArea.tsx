a6030fffa6b57e1193a951ac9506a85c2f1ffa66
Vinay Srampickal Joseph <srampickaljoseph.v@husky.neu.edu>
2020-11-25 13:55:04 -0500

    implemented previous and next buttons in compare page

--- plage-watch/src/components/codecomparison/CodeArea.tsx
+++ plage-watch/src/components/codecomparison/CodeArea.tsx
@@ -2,35 +2,22 @@ import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import CodePage from './CodePage';
 import ReactSpeedometer from "react-d3-speedometer";
-import * as fs from 'fs';
+
 function CodeArea(props: any) {
-
-    const readFiles = (path:any)=>{
-    //    const  selectedFile = "./Sample1.js";
-    // const reader = new FileReader();
-    // reader.onload = (e) => {
-    //     const text = reader.result.toString().trim();
-    //     console.log(text);
-    // }
-    // reader.readAsText("./Sample1.js");
-        const text = fs.readFileSync('./Sample1.js');
-        console.log(text)
-
-    }
 
     return (
         <>
             <Row>
                 <Col md={5}>
                     <CodePage
-                        code={readFiles('')}
+                        code={props.plagiarism_data.submission1[props.plagiarism_data[props.index].submission1.file]}
                         filename={props.plagiarism_data[props.index].submission1.file}
                         plagiarized_code_lines={props.plagiarism_data[props.index].submission1.lines}
                     />
                 </Col>
                 <Col md={2} className='my-auto speedometer'>
                 <ReactSpeedometer
-                 value={50}
+                 value={props.plagiarism_data.score}
                  maxValue={100}
                 //  fluidWidth={true}
                 width={180}
@@ -39,7 +26,7 @@ function CodeArea(props: any) {
                     </Col>
                 <Col md={5}>
                     <CodePage
-                        code={readFiles(props.plagiarism_data[props.index].submission2.file)}
+                        code={props.plagiarism_data.submission2[props.plagiarism_data[props.index].submission2.file]}
                         filename={props.plagiarism_data[props.index].submission2.file}
                         plagiarized_code_lines={props.plagiarism_data[props.index].submission2.lines}
                     />
--- plage-watch/src/components/codecomparison/CodeCompare.tsx
+++ plage-watch/src/components/codecomparison/CodeCompare.tsx
@@ -3,14 +3,11 @@ import { Button, Row, Col } from 'react-bootstrap';
 import './CodeCompare.css'
 import CodeArea from './CodeArea';
 
+const disabled: boolean = false;
+const enabled: boolean = true;
+const firstPage: number = 1;
+
 interface CodeState {
-    // code1: string;
-    // code2: string;
-    // filename1: string;
-    // filename2: string;
-    // plagiarized_code1_lines: Array<number>;
-    // plagiarized_code2_lines: Array<number>;
-    // score: number;
     index: number
     prev_button: boolean;
     next_button: boolean;
@@ -23,89 +20,144 @@ class CodeCompare extends React.Component<{}, CodeState> {
         super(props)
         this.state =
         {
-            index: 1,
-            prev_button: false,
-            next_button: true,
+            index: firstPage,
+            prev_button: disabled,
+            next_button: disabled,
             plagiarism_data: this.getData(),
-            plagiarism_count: 0,
+            plagiarism_count: Object.keys(this.getData()).length - 3,
         };
+        this.nextButtonClick = this.nextButtonClick.bind(this);
+        this.previousButtonClick = this.previousButtonClick.bind(this);
     }
 
     getData(): {} {
         return {
             "1": {
-              "submission1": {
-                "file": "./src/test1/Sample1.js",
-                "lines": [
-                  1,  2,  3,  4,  5,  6,  7,
-                  8,  9, 10, 11, 12, 13, 14,
-                  15, 16, 17
-                ] },
-              "submission2":{
-                "file":",./src/test2/Sample4.js",
-                "lines": [
-                1,  2,  3,  4,  5,  6,  7,
-                8,  9, 10, 11, 12, 13, 14,
-                15, 16, 17
-              ]
-              }},
+                "submission1": {
+                    "file": "./src/test1/Sample1.js",
+                    "lines": [
+                        1,
+                        2,
+                        3,
+                        4,
+                        5,
+                        6,
+                        7,
+                        8,
+                        9,
+                        10,
+                        11,
+                        12,
+                        13,
+                        14,
+                        15,
+                        16,
+                        17
+                    ]
+                },
+                "submission2": {
+                    "file": "./src/test2/Sample4.js",
+                    "lines": [
+                        1,
+                        2,
+                        3,
+                        4,
+                        5,
+                        6,
+                        7,
+                        8,
+                        9,
+                        10,
+                        11,
+                        12,
+                        13,
+                        14,
+                        15,
+                        16,
+                        17
+                    ]
+                }
+            },
             "2": {
-              "submission1": {
-                "file": "./src/test1/Sample2.js",
-                "lines": [
-                  10,
-                  11
-                ]
-              },
-              "submission2": {
-                "file": "./src/test2/Sample3.js",
-                "lines": [ 11, 12 ]
-            }},
+                "submission1": {
+                    "file": "./src/test1/Sample2.js",
+                    "lines": [
+                        10,
+                        11
+                    ]
+                },
+                "submission2": {
+                    "file": "./src/test2/Sample3.js",
+                    "lines": [
+                        11,
+                        12
+                    ]
+                }
+            },
             "3": {
-              "submission1": {
-                "file": "./src/test1/Sample2.js",
-                "lines": [ 3, 4, 5 ]},
-              "submission2": {
-                "file": ",./src/test2/Sample4.js",
-                "lines": [ 6, 7, 8 ]
-            }},
+                "submission1": {
+                    "file": "./src/test1/Sample2.js",
+                    "lines": [
+                        3,
+                        4,
+                        5
+                    ]
+                },
+                "submission2": {
+                    "file": "./src/test2/Sample4.js",
+                    "lines": [
+                        6,
+                        7,
+                        8
+                    ]
+                }
+            },
+            "submission1": {
+                "./src/test1/Sample1.js": "function binarySearch(myArray, value){\r\n    let left = 0;\r\n    let right = myArray.length - 1;\r\n    let middle = parseInt((myArray.length - 1)/2)\r\n    while (left <= right) {\r\n        if (myArray[middle] === value) {\r\n            return middle;\r\n        } else if (myArray[middle] < value) {\r\n            left = middle + 1;\r\n            middle = parseInt((left + right)/2);\r\n        } else if (myArray[middle] > value) {\r\n            right = middle - 1;\r\n            middle = parseInt((left + right)/2);\r\n        }\r\n    }\r\n    return -1;\r\n}\r\nconsole.log(binarySearch([1,2,3,4,5,6,7,16],51))",
+                "./src/test1/Sample2.js": "// Naive fibonacci.\r\nfunction fibonacci(n) {\r\n  if (n <= 1) {\r\n      return n;\r\n  } else {\r\n      return fibonacci(n-1) + fibonacci(n-2)\r\n  }\r\n}\r\n// plagiarism in the test case\r\nvar copied1 = fibonacci(50);\r\nconsole.log(copied1);"
+            },
+            "submission2": {
+                "./src/test2/Sample3.js": "// Fibonacci iterative.\r\nfunction fibonacci(n) {\r\n  // memoization array\r\n  let arr = [0,1];\r\n      for (let i = 2; i < n + 1; i++) {\r\n          arr.push(arr[i-2] + arr[i-1])\r\n      }\r\n      return arr[n];\r\n}\r\n// plagiarism in the test case\r\nlet copy1 = fibonacci(50);\r\nconsole.log(copy1);",
+                "./src/test2/Sample4.js": "function BinarySearch(myArray, value){\r\n    let l = 0;\r\n    let r = myArray.length - 1;\r\n    let mid = parseInt((myArray.length - 1)/2)\r\n    while (l <= r) {\r\n        if (myArray[mid] === value) {\r\n            return mid;\r\n        } else if (myArray[mid] < value) {\r\n            l = mid + 1;\r\n            mid = parseInt((l + r)/2);\r\n        } else if (myArray[mid] > value) {\r\n            r = mid - 1;\r\n            mid = parseInt((l + r)/2);\r\n        }\r\n    }\r\n    return -1;\r\n}\r\nconsole.log(BinarySearch([4,5,6,7,7,8,9],10))"
+            },
             "score": 75.86206896551724
-          }
+        }
     }
 
     componentDidMount() {
-        this.setState({plagiarism_data: this.getData()});
-        this.setState({index:1})   ;
-        this.setState({plagiarism_count:Object.keys(this.getData()).length });
-        if (this.state.index===1) {
-            this.setState({prev_button:false});
+        let { index, plagiarism_count } = this.state;
+        if (index < plagiarism_count) {
+            this.setState({ next_button: enabled })
         }
-        if(this.state.index===this.state.plagiarism_count) {
-            this.setState({next_button: false});
-        }     
+        if (index > firstPage) {
+            this.setState({ prev_button: enabled })
+        }
     }
 
     nextButtonClick() {
         const { index, plagiarism_count } = this.state;
         if (index <= plagiarism_count) {
-            this.setState({ index: index - 1 });
+            this.setState({ index: index + 1 });
         }
-        if (index === plagiarism_count) {
-            this.setState({ next_button: false });
-        } else {
-            this.setState({ next_button: true });
+        if (index > firstPage - 1) {
+            this.setState({ prev_button: enabled });
         }
+        if (index == plagiarism_count - 1) {
+            this.setState({ next_button: disabled })
+        }
+
     }
 
     previousButtonClick() {
-        const { index } = this.state;
-        if (index > 1) {
+        const { index, plagiarism_count } = this.state;
+        if (index > firstPage) {
             this.setState({ index: index - 1 });
         }
-        if (index===1) {
-            this.setState({prev_button:false});
-        }else{
-            this.setState({prev_button:true});
+        if (index <= firstPage + 1) {
+            this.setState({ prev_button: disabled });
+        }
+        if (index == plagiarism_count) {
+            this.setState({ next_button: enabled })
         }
     }
 
@@ -163,13 +215,7 @@ class CodeCompare extends React.Component<{}, CodeState> {
                     </Col>
                 </Row>
                 <CodeArea
-                    // code1={this.state.code1}
-                    // code2={this.state.code2}
-                    // filename1={this.state.filename1}
-                    // filename2={this.state.filename2}
-                    // plagiarized_code1_lines={this.state.plagiarized_code1_lines}
-                    // plagiarized_code2_lines={this.state.plagiarized_code2_lines}
-                    index = {this.state.index}
+                    index={this.state.index}
                     plagiarism_data={this.state.plagiarism_data}
 
                 />
