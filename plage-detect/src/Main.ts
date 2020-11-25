51b734c7977274fb3ffcdaef1298fabab67cf2d5
Vinay Srampickal Joseph <srampickaljoseph.v@husky.neu.edu>
2020-11-24 19:38:40 -0500

    updated the JSON object to return the content of files

--- plage-detect/src/Main.ts
+++ plage-detect/src/Main.ts
@@ -6,17 +6,19 @@ const fs = require('fs')
 
 export default function runPlagiarism(){
 
-    let sub1RootNodes = []
-    let sub2RootNodes = []
-    let sub1FileNameMap = {}
-    let sub2FileNameMap = {}
+    let sub1RootNodes = [];
+    let sub2RootNodes = [];
+    let sub1FileNameMap = {};
+    let sub2FileNameMap = {};
+    let fileSubmission1 = {};
+    let fileSubmission2 = {};
     
     let sub1AllNodes:Array<Array<any>>
     let sub2AllNodes:Array<Array<any>>
 
     let generator = new ASTNodeGenerator()
-    sub1RootNodes = generator.generateASTNodes('./src/test1', sub1FileNameMap)
-    sub2RootNodes = generator.generateASTNodes('./src/test2', sub2FileNameMap)
+    sub1RootNodes = generator.generateASTNodes('./src/test1', sub1FileNameMap, fileSubmission1)
+    sub2RootNodes = generator.generateASTNodes('./src/test2', sub2FileNameMap, fileSubmission2)
 
 
     let collectNodes = new CollectNodes()
@@ -24,7 +26,7 @@ export default function runPlagiarism(){
     sub2AllNodes = collectNodes.collectNodes(sub2RootNodes)
     
 
-    let plagiarismDetector = new DetectPlagiarism(sub1AllNodes, sub2AllNodes,sub1FileNameMap, sub2FileNameMap);
+    let plagiarismDetector = new DetectPlagiarism(sub1AllNodes, sub2AllNodes,sub1FileNameMap, sub2FileNameMap, fileSubmission1, fileSubmission2);
     let result = plagiarismDetector.detect()
 
     return result
