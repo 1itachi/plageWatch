bf4964563d12fd0ae9be94e5cc7572723e653238
deepak <kumar.de@husky.neu.edu>
2020-11-30 14:01:06 -0500

    fix alignment

--- plage-watch/src/components/footer/Footer.tsx
+++ plage-watch/src/components/footer/Footer.tsx
@@ -7,11 +7,11 @@ export default function  Footer () {
                 <div className="col-sm-6 offset-sm-1">
                     <p className= "footer-header col-sm-4 center"><u>CONTRIBUTORS</u></p>
                     <div className="row">
-                    <a className= "col-sm-3 footer-element mr-3" href="https://www.linkedin.com/in/deepak-kumar-bb1810115/">Deepak Kumar <i className="fab fa-linkedin"></i></a>
+                    <a className= "col-sm-3 footer-element mr-3 ml-3" href="https://www.linkedin.com/in/deepak-kumar-bb1810115/">Deepak Kumar <i className="fab fa-linkedin"></i></a>
                     <a className= "col-sm-3 offset-sm-0 footer-element" href="#">Jason Lu</a>
                     </div>
                     <div className="row">
-                    <a className= "col-sm-3 footer-element mr-3" href="https://www.linkedin.com/in/sanket-ghanmare/"> Sanket Ghanmare<i className="fab fa-linkedin"></i></a>
+                    <a className= "col-sm-3 footer-element mr-3 ml-3" href="https://www.linkedin.com/in/sanket-ghanmare/"> Sanket Ghanmare<i className="fab fa-linkedin"></i></a>
                     <a className= "col-sm-3 footer-element" href="https://www.linkedin.com/in/vinaysj/">Vinay S.Joseph<i className="fab fa-linkedin"></i></a>
                     </div>
                 </div>
